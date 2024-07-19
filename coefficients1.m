b1aa2d25e36c56442222a21a7fd5788f6f4de360
Sadra <motgolham@gmail.com>
2024-07-19 16:09:38 +0330

    Update coefficients1.m

--- coefficients1.m
+++ coefficients1.m
@@ -2,13 +2,12 @@ clc;
 clear;
 close all;
 format rat;
-k=10;
+k=2;
 h=1/k;
-
 %Computing  Newton-Cotes rules coefficients for k
 syms y r
 R=zeros(k,k+1);
-ww=zeros(k,k+1);
+coefficients=zeros(k,k+1);
 g=@(y,r) y.^r;
 I=0:1/k:1;
 C=I;
@@ -20,7 +19,6 @@ for j=1:k
     for i=0:k
         C(i+1)=int(g(y,i),y,0,j*1/k);
     end
-    ww(j,:)=(R\C')';
+    coefficients(j,:)=(R\C')';
 end
-w=k*h*ww;
-%Computing  Newton-Cotes rules coefficients for k
+coefficients=coefficientscoefficients*k
